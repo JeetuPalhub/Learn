e9fd9c49e94252c54409726fbe01dbf4684eab97
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-12-18 19:22:00 +0530

    sync code

---
+++ index.js
@@ -0,0 +1,18 @@
+//sync code
+const data= fs.readFileSync("file.txt");
+console.log(data);
+
+const data1 = fs.readFileSync("file.txt");
+console.log(data1);
+
+const data2 = fs.readFileSync("file.txt");
+console.log(data2);
+
+const data3 = fs.readFileSync("file.txt");
+console.log(data3);
+
+const data4 = fs.readFileSync("file.txt");
+console.log(data4);
+
+const data5 = fs.readFileSync("file.txt");
+console.log(data5);
